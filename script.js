cd3253ff3c528e63aa083a2de17877c935d7058d
Michal.Bac <michal.bac1993@gmail.com>
2019-02-06 16:34:04 +0100

    Second commit

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-  const apiRoot = 'http://localhost:8080/v1/task';
+  const apiRoot = 'http://localhost:8080/v1/';
   const trelloApiRoot = 'https://pure-thicket-73593.herokuapp.com/v1/trello/';
   const datatableRowTemplate = $('[data-datatable-row-template]').children()[0];
   const $tasksContainer = $('[data-tasks-container]');
